2999ff2cc8ae8549d6337c957e4f9f2194319855
saruncr <saruncr@gmail.com>
2016-09-28 21:55:23 +0530

    lachus of commit

---
+++ student/index.php
@@ -0,0 +1,37 @@
+<html>
+    title TODO supply a title
+    <body>
+   <? php
+   <table style="width:100%",border=solid black>
+   
+   <form action="index.php" method="post">
+   <tr>
+  <th>
+  first name: <input type="text" name="fname"> <br>
+  last name: <input type="text" name="lname"> <br>
+</th>
+</tr>
+ <tr>
+ <td>
+ Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male <br>
+e mail:<inputtype="text" name="email"> <br>
+</td>
+</tr>
+<tr>
+ <td>submit:<input type="submit">
+ </td>
+ </tr>
+  </form>
+  </table>
+  ?>
+    </body>
+</html>
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
