811286389be0438356e0698c59a3ae3f17a1e01d
saruncr <saruncr@gmail.com>
2016-10-01 17:41:51 +0530

    lachus 2nd commit

--- student/index.php
+++ student/index.php
@@ -1,7 +1,18 @@
+<?php
+if((isset($_POST['submit']))&&($_POST['submit'] == 'submit')){
+    print_r($_POST);
+
+    echo "stop Here";
+    exit;
+}
+
+
+?>
+
 <html>
     title TODO supply a title
     <body>
-   <? php
+  
    <table style="width:100%",border=solid black>
    
    <form action="index.php" method="post">
@@ -16,22 +27,16 @@
  Gender:
 <input type="radio" name="gender" value="female">Female
 <input type="radio" name="gender" value="male">Male <br>
-e mail:<inputtype="text" name="email"> <br>
+e mail:<input type="text" name="email"> <br>
 </td>
 </tr>
 <tr>
- <td>submit:<input type="submit">
+ <td>submit:<input type="submit" value="submit" name="submit">
  </td>
  </tr>
   </form>
   </table>
-  ?>
+  
     </body>
 </html>
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
---
+++ student/my project.php
@@ -0,0 +1,63 @@
+
+<html>
+<head>
+</head>
+<body>  
+
+
+
+
+<?php
+
+$firstname = $lastname=$email = $gender  = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $firstname = ($_POST["fname"]);
+    $lastname = ($_POST["lname"]);
+    $gender = ($_POST["gender"]);
+  
+  $email = ($_POST["email"]);
+  
+}
+
+
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+ <table style="width:100%",border=solid black>
+     <tr>
+  <th>
+  first name: <input type="text" name="fname"> <br>
+  last name: <input type="text" name="lname"> <br>
+</th>
+</tr>
+ <tr>
+ <td>
+ Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male <br>
+e mail:<input type="text" name="email"> <br>
+</td>
+</tr>
+<tr>
+ <td>submit:<input type="submit" value="submit" name="submit">
+ </td>
+ </tr>
+ 
+  </table>
+</form>
+
+<?php
+
+echo "my firstname is $firstname";
+echo "<br>";
+echo "my lastname is $lastname";
+echo "<br>";
+echo "my gender is $gender";
+echo "<br>";
+echo "my email is $email";
+
+?>
+
+</body>
+</html>
